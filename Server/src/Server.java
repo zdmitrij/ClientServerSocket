52c9c8f9094923d72646a0164a0940c7b69797d0
Dmitriy Zadorozhnyy <zdmitrij@yandex.ru>
2023-06-27 19:43:05 +0300

    2023-06-27 урок с YOUTUBE  Создание многопоточного клиент-серверного приложения на Java ДО 1.09.59 РАБОТАЕТ

---
+++ Server/src/Server.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+
+        try (
+                ServerSocket server = new ServerSocket(8000)) {
+            System.out.println("Server started!");
+
+            while (true)
+                try (
+                        Socket socket = server.accept();
+                        BufferedWriter writer =
+                                new BufferedWriter(
+                                        new OutputStreamWriter(
+                                                socket.getOutputStream()));
+                        BufferedReader reader =
+                                new BufferedReader(
+                                        new InputStreamReader(
+                                                socket.getInputStream()));
+
+                ) {
+                    String request = reader.readLine();
+                    System.out.println("Request  " + request);
+                    String response = (int)(Math.random() * 30 -10) + "";
+                    System.out.println("Response  " + response);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
