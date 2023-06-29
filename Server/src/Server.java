49fb532a4ce2639a557016e0b44a78a9458d5999
Dmitriy Zadorozhnyy <zdmitrij@yandex.ru>
2023-06-29 09:00:09 +0300

    2023-06-29 урок с YOUTUBE  Создание многопоточного клиент-серверного приложения на Java ДО 1.41.28 создан класс PHONE куда вынесены создания IO и сокетов РАБОТАЕТ

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,40 +1,27 @@
+import zadorozhny.Phone;
+
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
+
 
 public class Server {
     public static void main(String[] args) {
 
-        try (
-                ServerSocket server = new ServerSocket(8000)) {
+        try (ServerSocket server = new ServerSocket(8000)) {
             System.out.println("Server started!");
 
             while (true)
-                try (
-                        Socket socket = server.accept();
-                        BufferedWriter writer =
-                                new BufferedWriter(
-                                        new OutputStreamWriter(
-                                                socket.getOutputStream()));
-                        BufferedReader reader =
-                                new BufferedReader(
-                                        new InputStreamReader(
-                                                socket.getInputStream()));
-
-                ) {
-                    String request = reader.readLine();
+                try (Phone phone = new Phone(server)) {
+                    String request = phone.readLine();
                     System.out.println("Request  " + request);
-                    String response = (int)(Math.random() * 30 -10) + "";
+                    String response = (int) (Math.random() * 30 - 10) + "";
+                    phone.writeLine(response);
                     System.out.println("Response  " + response);
-                    writer.write(response);
-                    writer.newLine();
-                    writer.flush();
-                } catch (IOException e) {
+                } catch (NullPointerException e) {
                     e.printStackTrace();
                 }
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+                    throw new RuntimeException(e);
+                }
+            }
         }
-    }
-}
